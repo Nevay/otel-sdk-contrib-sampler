3e92f9a9d1db66a5eb3019c179ea7148ec38f705
Tobias Bachert <git@b-privat.de>
2024-03-29 03:08:23 +0100

    Use `Validation::ensure{...}()` methods

--- Configuration/SamplingRuleAttribute.php
+++ Configuration/SamplingRuleAttribute.php
@@ -1,20 +1,13 @@
 <?php declare(strict_types=1);
 namespace Nevay\OTelSDK\Contrib\Sampler\Configuration;
 
-use InvalidArgumentException;
 use Nevay\OTelSDK\Configuration\ComponentProvider;
 use Nevay\OTelSDK\Configuration\ComponentProviderRegistry;
 use Nevay\OTelSDK\Configuration\Context;
+use Nevay\OTelSDK\Configuration\Validation;
 use Nevay\OTelSDK\Contrib\Sampler\SamplingRule;
 use Nevay\OTelSDK\Contrib\Sampler\SamplingRule\AttributeRule;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
-use function is_string;
-use function preg_match;
-use function restore_error_handler;
-use function set_error_handler;
-use function str_starts_with;
-use function strlen;
-use function substr;
 
 final class SamplingRuleAttribute implements ComponentProvider {
 
@@ -38,39 +31,15 @@ final class SamplingRuleAttribute implements ComponentProvider {
                 ->scalarNode('key')
                     ->isRequired()
                     ->cannotBeEmpty()
-                    ->validate()->ifTrue(static fn($v) => !is_string($v))->thenInvalid('must be of type string')->end()
+                    ->validate()->always(Validation::ensureString())->end()
                 ->end()
                 ->scalarNode('pattern')
                     ->isRequired()
-                    ->validate()
-                        ->always(static function($value): string {
-                            if (!is_string($value)) {
-                                throw new InvalidArgumentException('must be a regex pattern');
-                            }
-
-                            set_error_handler(static fn(int $errno, string $errstr): never
-                                => throw new InvalidArgumentException('must be a valid regex pattern: ' . self::stripPrefix($errstr, 'preg_match(): '), $errno));
-                            try {
-                                preg_match($value, '');
-                            } finally {
-                                restore_error_handler();
-                            }
-
-                            return $value;
-                        })
-                    ->end()
+                    ->validate()->always(Validation::ensureRegexPattern())->end()
                 ->end()
             ->end()
         ;
 
         return $node;
-    }
-
-    private static function stripPrefix(string $string, string $prefix): string {
-        if (str_starts_with($string, $prefix)) {
-            return substr($string, strlen($prefix));
-        }
-
-        return $string;
     }
 }
--- Configuration/SamplingRuleSpanName.php
+++ Configuration/SamplingRuleSpanName.php
@@ -1,20 +1,13 @@
 <?php declare(strict_types=1);
 namespace Nevay\OTelSDK\Contrib\Sampler\Configuration;
 
-use InvalidArgumentException;
 use Nevay\OTelSDK\Configuration\ComponentProvider;
 use Nevay\OTelSDK\Configuration\ComponentProviderRegistry;
 use Nevay\OTelSDK\Configuration\Context;
+use Nevay\OTelSDK\Configuration\Validation;
 use Nevay\OTelSDK\Contrib\Sampler\SamplingRule;
 use Nevay\OTelSDK\Contrib\Sampler\SamplingRule\SpanNameRule;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
-use function is_string;
-use function preg_match;
-use function restore_error_handler;
-use function set_error_handler;
-use function str_starts_with;
-use function strlen;
-use function substr;
 
 final class SamplingRuleSpanName implements ComponentProvider {
 
@@ -35,35 +28,11 @@ final class SamplingRuleSpanName implements ComponentProvider {
             ->children()
                 ->scalarNode('pattern')
                     ->isRequired()
-                    ->validate()
-                        ->always(static function($value): string {
-                            if (!is_string($value)) {
-                                throw new InvalidArgumentException('must be a regex pattern');
-                            }
-
-                            set_error_handler(static fn(int $errno, string $errstr): never
-                                => throw new InvalidArgumentException('must be a valid regex pattern: ' . self::stripPrefix($errstr, 'preg_match(): '), $errno));
-                            try {
-                                preg_match($value, '');
-                            } finally {
-                                restore_error_handler();
-                            }
-
-                            return $value;
-                        })
-                    ->end()
+                    ->validate()->always(Validation::ensureRegexPattern())->end()
                 ->end()
             ->end()
         ;
 
         return $node;
-    }
-
-    private static function stripPrefix(string $string, string $prefix): string {
-        if (str_starts_with($string, $prefix)) {
-            return substr($string, strlen($prefix));
-        }
-
-        return $string;
     }
 }
